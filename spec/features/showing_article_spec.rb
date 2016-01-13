57b4b8832d737d5c27518d59de901781c94a8a76
railsmaster <supe.s.kunal@gmail.com>
2016-01-13 10:42:18 +0530

    restrict new edit and delete options for signed in users

--- spec/features/showing_article_spec.rb
+++ spec/features/showing_article_spec.rb
@@ -3,7 +3,43 @@ require 'rails_helper'
 RSpec.feature "Showing Article" do
 
   before do
-    @article = Article.create(title: "This is first Article", body: "Body of first article")
+    @john = User.create(email: "john@example.com", password: "password" )
+    @fred = User.create(email: "fred@example.com", password: "password" )
+    @article = Article.create(title: "This is first Article", body: "Body of first article", user: @john)
+  end
+
+  scenario "A non-signed in user doest not see Edit or Delete Links" do
+    visit '/'
+
+    click_link @article.title
+
+    expect(page).to have_content(@article.title)
+    expect(page).to have_content(@article.body)
+    expect(current_path).to eq(article_path(@article))
+    expect(page).not_to have_link("Edit Article")
+    expect(page).not_to have_link("Delete Article")
+  end
+
+  scenario "A non-owner signed in cannot see both links" do
+    login_as(@fred)
+
+    visit "/"
+
+    click_link @article.title
+
+    expect(page).not_to have_link("Edit Article")
+    expect(page).not_to have_link("Delete Article")
+  end
+
+  scenario "A signed in onwer can see both links" do
+    login_as(@john)
+
+    visit "/"
+
+    click_link @article.title
+
+    expect(page).to have_link("Edit Article")
+    expect(page).to have_link("Delete Article")
   end
 
   scenario "show article" do
