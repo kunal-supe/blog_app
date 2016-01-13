ca82851b69804419f52cb4c9b9d46ffb7faf4dc3
railsmaster <supe.s.kunal@gmail.com>
2016-01-13 09:45:44 +0530

    Link article to users

--- spec/features/creating_article_spec.rb
+++ spec/features/creating_article_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
 
 RSpec.feature "Creating Articles" do
+
+  before do
+    @john = User.create!(email: "john@example.com", password: "passowrd")
+    login_as(@john)
+  end
+
+
   scenario "A user creates a new article" do
     visit "/"
 
@@ -12,6 +19,7 @@ RSpec.feature "Creating Articles" do
 
     expect(page).to have_content("Article has been created")
     expect(page.current_path).to eq(articles_path)
+    expect(page).to have_content("Created by: #{@john.email}")
   end
 
   scenario "A user fails to create a new article" do
--- spec/features/deleting_article_spec.rb
+++ spec/features/deleting_article_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.feature "Deleting Articles" do
 
   before do
-    @article = Article.create(title: "This is first Article", body: "Body of first article")
+    john = User.create!(email: "john@example.com", password: "passowrd")
+    login_as(john)
+    @article = Article.create(title: "This is first Article", body: "Body of first article", user: john)
   end
 
   scenario "A user deletes an article" do
@@ -15,4 +17,4 @@ RSpec.feature "Deleting Articles" do
     expect(page).to have_content("Article has been deleted")
     expect(current_path).to eq(articles_path)
   end
-end  
+end
